93c660094fe96fce69be857722c2ae057391d8fe
dhksehdtj <dhksehdtj@naver.com>
2017-04-06 18:31:16 +0900

    add

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -1,11 +1,36 @@
 package univ.lecture;
+//dongwan
+
+import java.util.StringTokenizer;
 
 /**
  * Created by tchi on 2017. 3. 19..
  */
 public class Calculator {
-    public int calculate(String exp) {
-    	//dongwan
-        return 2;
-    }
+	public int calculate(String exp) {
+		
+		int num;
+		
+		String sum = "+";
+		String sub = "-";
+		String mul = "*";
+		String div = "/";
+
+		if (exp.contains(sum)) {
+			StringTokenizer token = new StringTokenizer(exp, sum);
+			
+				int temp = calculate(token.nextToken());
+			
+			while (token.hasMoreTokens()) {
+					String val = token.nextToken();
+					temp = temp + calculate(val);
+			}
+			
+				num = temp;
+			
+		} else {
+			num = Integer.parseInt(exp);
+		}
+		return num;
+	}
 }
